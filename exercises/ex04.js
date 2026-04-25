03ae9ea783332241b54bc8c44dbd904e0af25086
nakazmi-ak <asimness9348@gmail.com>
2026-04-24 22:26:58 -0700

    shrine to his silverhand

---
+++ exercises/ex04.js
@@ -0,0 +1,21 @@
+let count=0;
+let colors=["Orchid", "Coral", "HotPink", "Plum"];
+let colorCount;
+
+$("#needy-button").click( function(){
+
+    count = count +1; 
+
+    colorCount=count-1;
+
+    $("#needy-button").html("you clicked me " + count + " times, and your prophesized color is: " + colors[colorCount] );
+
+    $("#needy-button").css( "background-color", colors[colorCount]);  
+
+    $("html").css( "background-color", colors[colorCount+1]);
+
+// things to try out:
+// make the page background color two colors ahead of the button color
+// make the page background color one color beyond of the button color
+
+});
